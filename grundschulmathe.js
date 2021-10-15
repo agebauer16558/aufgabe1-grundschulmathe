9ab378fd2b28a7d56b536defbf4c7544214dfa7f
agebauer16558 <a.gebauer16558@hs-ansbach.de>
2021-10-15 16:44:29 +0200

    fulfilled task b

--- grundschulmathe.js
+++ grundschulmathe.js
@@ -1,1 +1,14 @@
-alert("Grundschulmathe")
+alert ("Grundschulmathe")
+
+var a = Math.floor(Math.random()*21)
+var b = Math.floor(Math.random()*21)
+var result = prompt (`Wie viel ist ${a} + ${b}?`)
+
+if (parseInt(result, 10) === a + b) 
+{
+    alert ("Richtig!")
+}
+else 
+{
+    alert ("Falsch!")
+}
